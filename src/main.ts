b11c344f23ef26cc4f85324bb87f2c6d91763d98
Deli Soetiawan <deli.soetiawan@mekari.com>
2024-07-15 09:53:11 +0700

    feat: configure validation & mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  mongoose.set('debug', process.env.MONGOOSE_DEBUG === 'true');
   await app.listen(3000);
 }
 bootstrap();
